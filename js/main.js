63f1c454f351f9a4145dfbf6eed3d62d573ff4c0
Chuck <GasPractice99@gmail.com>
2017-10-09 17:41:28 -0700

    end of part 6

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
         
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
@@ -24,13 +17,10 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
-    },
-
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+
+
 
 };
 
-app.initialize();
+app.initialize(); 
